ce484f3936a4f3d22cad60dfac94e28daf523abc
devjoelson <dev.joelsonamarante@gmail.com>
2025-06-15 22:06:27 -0300

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-const carouselElement = document.querySelector('#motoboyCarousel');
-
-if (carouselElement) {
-  const carousel = new bootstrap.Carousel(carouselElement, {
-    interval: 4000, // muda a cada 4 segundos
-    ride: 'carousel',
-    pause: 'hover'
-  });
-}
